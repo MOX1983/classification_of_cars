f329a57fc30b23bf6c61c3e265383b7a81168d71
Mox <tenmaken246@gmail.com>
2025-03-03 17:05:57 +0300

    библиотеки

--- Main.py
+++ Main.py
@@ -1,10 +1,15 @@
 import json
 import os
 from PIL import Image
+import numpy as np
 
 import torch
-import torchvision.transforms.v2 as tfs
+import torchvision
+from torchvision.datasets import ImageFolder
+from torch.utils.data import Dataset, DataLoader, random_split
 import torch.nn as nn
 import matplotlib.pyplot as plt
+import torch.utils.data as data
+import torch.optim as optim
 
 
