f8077530c3bb817e80d34cb03431398d3de5ae07
Mox <tenmaken246@gmail.com>
2025-03-03 16:58:37 +0300

    библиотеки

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
-import numpy as np
+import json
+import os
+from PIL import Image
+
+import torch
+import torchvision.transforms.v2 as tfs
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+
