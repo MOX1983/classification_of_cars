c5e2f22a8e2dbf8909887e350725c9b09301f8f8
Mox <tenmaken246@gmail.com>
2025-04-09 23:19:10 +0300

    изменила структуру для GUI

--- Test.py
+++ Test.py
@@ -1,37 +1,41 @@
-import os
 from PIL import Image
 
 import torch
 import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
 
 from Main import MyDataset, MyModel
 
+def run(path):
+    transform = transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.ToTensor()
+    ])
+
+    model = MyModel(3, 2)
+    model.load_state_dict(torch.load('model.pt'))
+
+    img = Image.open(path).convert('RGB')
+    img_t = transform(img).unsqueeze(0)
+
+    model.eval()
+    with torch.no_grad():
+        output = model(img_t)
+        predicted_class = output.argmax(dim=1).item()
+
+    clss = ["car", "truck"]
+    # print(f"Predicted class: {clss[predicted_class]}")
+    # plt.imshow(img)
+    # plt.show()
+    return "Predicted class: " + clss[predicted_class]
+
+
+# path = '.\\Datasets\\valid\\car\\05392.jpeg'
+# run(path)
+
+
 # path = '.\\Datasets\\valid\\Truck\\'
 # num = 'Datacluster Truck (135).jpg'
-path = '.\\Datasets\\valid\\car\\'
-num = '05392.jpeg'
 
-transform = transforms.Compose([
-    transforms.Resize((128, 128)),
-    transforms.ToTensor()
-])
-
-model = MyModel(3, 2)
-model.load_state_dict(torch.load('model.pt'))
-
-img = Image.open(os.path.join(path, num)).convert('RGB')
-img_t = transform(img).unsqueeze(0)
-
-model.eval()
-with torch.no_grad():
-    output = model(img_t)
-    predicted_class = output.argmax(dim=1).item()
-
-clss = ["car", "truck"]
-print(f"Predicted class: {clss[predicted_class]}")
-plt.imshow(img)
-plt.show()
 
 # test = {pathT : ['Datacluster Truck (75).jpg', 'Datacluster Truck (94).jpg', 'Datacluster Truck (31).jpg', 'Datacluster Truck (42).jpg'],
 #         pathC : ['05138.jpeg', '05143.jpeg', '06784.jpeg', '06912.jpeg']}
