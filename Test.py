2a643c4b37bc787f510cbf9a9a52a05b8dbe53b3
Mox <tenmaken246@gmail.com>
2025-04-09 22:19:53 +0300

    проверка

--- Test.py
+++ Test.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 
 from Main import MyDataset, MyModel
 
-pathT = '.\\Datasets\\valid\\truck\\'
-# num = '10167'
-pathC = '.\\Datasets\\valid\\car\\'
-# num = '05118'
+# path = '.\\Datasets\\valid\\Truck\\'
+# num = 'Datacluster Truck (135).jpg'
+path = '.\\Datasets\\valid\\car\\'
+num = '05392.jpeg'
 
 transform = transforms.Compose([
     transforms.Resize((128, 128)),
@@ -20,18 +20,32 @@ transform = transforms.Compose([
 model = MyModel(3, 2)
 model.load_state_dict(torch.load('model.pt'))
 
-test = {pathT : ['05117', '06513', '07602', '10105'], pathC : ['05138', '05143', '06784', '06912']}
-for key, val in test.items():
-    for i in val:
-        img = Image.open(os.path.join(key, f'{i}.jpeg')).convert('RGB')
-        img_t = transform(img).unsqueeze(0)
+img = Image.open(os.path.join(path, num)).convert('RGB')
+img_t = transform(img).unsqueeze(0)
 
-        model.eval()
-        with torch.no_grad():
-            output = model(img_t)
-            predicted_class = output.argmax(dim=1).item()
+model.eval()
+with torch.no_grad():
+    output = model(img_t)
+    predicted_class = output.argmax(dim=1).item()
 
-        clss = ["car", "truck"]
-        print(f"Predicted class: {clss[predicted_class]}")
-        plt.imshow(img)
-        plt.show()
+clss = ["car", "truck"]
+print(f"Predicted class: {clss[predicted_class]}")
+plt.imshow(img)
+plt.show()
+
+# test = {pathT : ['Datacluster Truck (75).jpg', 'Datacluster Truck (94).jpg', 'Datacluster Truck (31).jpg', 'Datacluster Truck (42).jpg'],
+#         pathC : ['05138.jpeg', '05143.jpeg', '06784.jpeg', '06912.jpeg']}
+# for key, val in test.items():
+#     for i in val:
+#         img = Image.open(os.path.join(key, i)).convert('RGB')
+#         img_t = transform(img).unsqueeze(0)
+#
+#         model.eval()
+#         with torch.no_grad():
+#             output = model(img_t)
+#             predicted_class = output.argmax(dim=1).item()
+#
+#         clss = ["car", "truck"]
+#         print(f"Predicted class: {clss[predicted_class]}")
+#         plt.imshow(img)
+#         plt.show()
