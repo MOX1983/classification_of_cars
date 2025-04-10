8ac5a0b10ab4fff9ed1ac8c51fdea4ab72970b71
Mox <tenmaken246@gmail.com>
2025-04-10 10:28:17 +0300

    изменила GUI

--- GUI.py
+++ GUI.py
@@ -13,8 +13,9 @@ def open_file():
     global filepath
     filepath = filedialog.askopenfilename()
     if filepath != "":
-        canvas = tkinter.Canvas(root, height=300, width=300)
+        canvas = tkinter.Canvas(frame, height=300, width=300)
         image = Image.open(filepath)
+        image = image.resize((200, 200))
         photo = ImageTk.PhotoImage(image)
         canvas.create_image(0, 0, anchor='nw', image=photo)
         canvas.grid(row=1, column=0)
@@ -28,13 +29,20 @@ root = tkinter.Tk()
 root.title("car or truck")
 root.geometry("500x300")
 
-bttn = tkinter.Button(text="Запустить", command=show_answer)
+frame = tkinter.Frame(
+    root,
+    padx=10,
+    pady=10
+)
+frame.pack(anchor="nw")
+
+bttn = tkinter.Button(frame, text="Запустить", command=show_answer)
 bttn.grid(row=0, column=1)
 
-open_button = ttk.Button(text="Открыть файл", command=open_file)
+open_button = ttk.Button(frame, text="Открыть файл", command=open_file)
 open_button.grid(column=0, row=0)
 
-lbl = tkinter.Label(text='')
+lbl = tkinter.Label(frame, text='')
 lbl.grid(row=1, column=1)
 
 
