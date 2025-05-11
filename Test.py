51ff3175a2011ce70c76b75de53ff31f76776574
Mox <tenmaken246@gmail.com>
2025-05-11 18:39:38 +0300

    поменла откуда импортируеться модель и удрала проверку

--- Test.py
+++ Test.py
@@ -3,7 +3,7 @@ from PIL import Image
 import torch
 import torchvision.transforms as transforms
 
-from Main import MyDataset, MyModel
+from traing.Main import MyModel
 
 def run(path):
     transform = transforms.Compose([
@@ -26,11 +26,3 @@ def run(path):
 
 
     return "Predicted class: " + clss[predicted_class]
-
-
-for i in [".\\Datasets\\valid\\bike\\4290.jpg",
-          ".\\Datasets\\valid\\bus\\bus-32_jpg.rf.e497b8fe649df811691e65d05a28ff0d.jpg",
-          ".\\Datasets\\valid\\car\\03444.jpeg",
-          ".\\Datasets\\valid\\motorbike\\46_sym_elite_50_vmse_1568719361305_11139.jpg",
-          ".\\Datasets\\valid\\truck\\Image_013471_jpg.rf.4fee3b4ad830a3e9bb36d8237d148cdd.jpg"]:
-    print(run(i))
